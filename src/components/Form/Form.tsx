cc142d73482821efc9bdab89b14c8842bb783449
Leo en laptop <leonar2zb@gmail.com>
2025-01-08 12:28:58 -0300

    Trabajando con el state

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,17 +1,40 @@
 import styles from './Form.module.css'
 import { countries } from '../../data/countries'
+import { ChangeEvent, useState } from 'react'
+import { SearchType } from '../../types'
+
 export default function Form() {
+    const [search, setSearch] = useState<SearchType>({
+        city: '',
+        country: ''
+    })
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setSearch({
+            ...search,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return (
         <form className={styles.form}>
             <div className={styles.field}>
                 <label htmlFor="city">Ciudad:</label>
                 <input type="text"
                     name='city'
-                    placeholder='Ciudad' />
+                    placeholder='Ciudad'
+                    id='city'
+                    value={search.city}
+                    onChange={handleChange}
+                />
             </div>
             <div className={styles.field}>
                 <label htmlFor="country">País:</label>
-                <select name="country">
+                <select name="country"
+                    id='country'
+                    value={search.country}
+                    onChange={handleChange}
+                >
                     <option value="">--Seleccione país--</option>
                     {countries.map(country => (
                         <option value={country.code} key={country.code}>{country.name}</option>
